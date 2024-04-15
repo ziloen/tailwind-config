f09a075a183c252ceed0a5b279ce585438f480b4
N <34268416+ziloen@users.noreply.github.com>
2024-04-15 15:02:25 +0800

    Use tsup build

---
+++ tsup.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "tsup"
+
+export default defineConfig({
+  entry: [
+    "src/index.ts",
+  ],
+  outDir: "dist",
+  // minify: true,
+  dts: true,
+  format: ["esm"],
+  platform: "node",
+  treeshake: true,
+  sourcemap: true,
+  clean: true,
+})
